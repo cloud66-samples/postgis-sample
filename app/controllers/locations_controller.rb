c6dbe7512bdb34939f5a62ff1506a43e0e935efc
Vic van Gool <vic@cloud66.com>
2018-02-14 14:19:03 +0000

    added read/write

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,14 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
   end
+
+  def read
+  	@location_count = Location.count
+  end
+
+  def write
+  	location_count = Location.count
+	Location.create name: "Test-#{@location_count}", point: RGeo::Geographic.spherical_factory(srid: 4326).point(174.7730682333, -41.2917018833)
+	@location_count = location_count + 1
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 PostgisSample::Application.routes.draw do
   root 'locations#index'
+
+  get 'read', :to => 'locations#read'
+  get 'write', :to => 'locations#write'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
